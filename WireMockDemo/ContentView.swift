3431078fd427086a01a5e41c7d92cd019dde5ae2
Tony Chen <1qazster@gmail.com>
2024-11-17 10:48:44 +0800

    First commit

--- WireMockDemo/ContentView.swift
+++ WireMockDemo/ContentView.swift
@@ -3,15 +3,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userName: String = ""
+    @State private var responseMsg: String = ""
+    
+#if DEBUG
+    let url = "http://localhost:1234/login"
+#else
+    let url = "http://real_backend_url/login"
+#endif
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            TextField("User name", text: $userName)
+            Button("Login") {
+                Task {
+                    let message = await sendLoginRequest(userName: userName)
+                    self.responseMsg = message
+                }
+            }
+            Text(responseMsg)
         }
         .padding()
     }
+    
+    func sendLoginRequest(userName: String) async -> String {
+        // Encode user data
+        let user = User(name: userName)
+        guard let encoded = try? JSONEncoder().encode(user) else {
+            print("Failed to encode user")
+            return ""
+        }
+        
+        // Construct POST request to login url
+        let url = URL(string: url)!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        // Send POST request to login url and handle the response
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let response = String(data: data, encoding: .utf8)!
+            return response
+        } catch {
+            print("Login failed: \(error.localizedDescription)")
+            return ""
+        }
+    }
+}
+
+struct User: Encodable {
+    let name: String
 }
 
 #Preview {
